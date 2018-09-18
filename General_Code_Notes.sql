d04fbfe8e2905d9f8c44e262ad247675c609f264
Lydia Tsui <xul3@sutterhealth.org>
2018-09-18 16:04:57 +0000

    Update General_Code_Notes.sql

--- General_Code_Notes.sql
+++ General_Code_Notes.sql
@@ -36,16 +36,7 @@ union
 select distinct new_pat_id as pat_id, old_pat_id from #pat_v2) as a
 ----------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------
-/*
-now we have a setup that automatically upload data to a database RDD_mrt on DCPWDBS149 from  a database RDD_to_149 on DCQWDBS088 daily at 6am. 
-Only our team (with CLARITY access) has read only permission to the database RDD_mrt. 
-Similarly, Ken and Clarity_system has read only access to our database RDD_to_149 .
-Append your data to a database RDD_to_149 on our server DCQWDBS088.
-Right now there are two tables RDD_ENC_CSN and RDD_HSP_ACCT. 
-If we decide to add new tables/ variables, we should work with DBA to update the setup.
-You can use the code below to append data to our database(RDD_to_149). 
-we can only upload pat_id (plus encounter id and hospital id), nothing else.
-*/
+
 insert into RDD_ENC_CSN (pat_id, PAT_ENC_CSN_ID, PROJECT_DATE, PROJECT_ID)
 select  pat_id,pat_enc_csn_id, 'upload date', 'your unique project id' as project_id from 'Your_Table_name'
 
